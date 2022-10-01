7942296e5a7ea66c8a7810adae5f9163e34ea176
DeseeBell <valentin.volatiles@gmail.com>
2022-10-01 04:22:48 -0300

    Added main menu

---
+++ BigFoot/scripts/main_menu.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_TutorialButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://scenes/game.tscn")
+	pass # Replace with function body.
+
+
+func _on_SettingsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_CreditsButton_pressed():
+	pass # Replace with function body.
